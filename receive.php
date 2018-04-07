92e416a424d7d449fe221e42f7c7eae5b463add3
Geol2 <big9401@gmail.com>
2018-04-08 03:11:45 +0900

    change localhost ip
    :+1:

--- receive.php
+++ receive.php
@@ -4,7 +4,7 @@
     require_once __DIR__ . '/library/vendor/autoload.php';
     use PhpAmqpLib\Connection\AMQPStreamConnection;
 
-    $connection = new AMQPStreamConnection('localhost', 5672, 'guest', 'guest');
+    $connection = new AMQPStreamConnection('203.250.32.169', 5672, 'guest', 'guest');
     $channel = $connection->channel();
 
     $channel->queue_declare('hello', false, false, false, false);
