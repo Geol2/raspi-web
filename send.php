fb5af896a7291a2c9bcae7908544f2b5b8422d2b
Geol2 <big9401@gmail.com>
2018-04-08 04:33:48 +0900

    haha

--- send.php
+++ send.php
@@ -12,25 +12,26 @@
         use PhpAmqpLib\Connection\AMQPStreamConnection;
         use PhpAmqpLib\Message\AMQPMessage;
 
-            try{
+            try
+            {
                 $param1 = $_POST['param1'];
 
+                if(!$param1) {
+                    throw new exception("No value param1.");
+                }
 
-                      if(!$param1) {
-
-                        throw new exception("No value param1.");
-                  }
-
-                    $result['success'] = true;
-                //$url = "203.250.32.128:5672";
-
-                } catch(exception $e) {
+                $result['success'] = true;
+            }
+            catch(exception $e)
+            {
 
                   $result['success'] = false;
                   $result['msg'] = $e->getMessage();
                   $result['code'] = $e->getCode();
 
-           } finally {
+           }
+           finally
+           {
 
                 //echo json_encode($result, JSON_PRETTY_PRINT|JSON_UNESCAPED_UNICODE);
 
@@ -46,7 +47,7 @@
 
                 $channel->close();
                 $connection->close();
-//            }
+            }
         ?>
     </body>
 </html>
