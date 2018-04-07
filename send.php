8c45ce0f6237d53d9c342b9b35d391ab63547daa
Geol2 <big9401@gmail.com>
2018-04-08 00:26:48 +0900

    RabbitMQ !!
    OK?

--- send.php
+++ send.php
@@ -8,16 +8,21 @@
 
     <body>
         <?php
+        require_once __DIR__ . '/var/www/html/send.php';
+        use PhpQmqLib\Connection\AMQPStreamConnection;
+        use PhpAmqLib\Message\AMQPMessage;
+
             try{
                 $param1 = $_POST['param1'];
+
 
                 if(!$param1) {
                     throw new exception("No value param1.");
                 }
 
                 $result['success'] = true;
-                $url = "203.250.32.128:5672";
-                
+                //$url = "203.250.32.128:5672";
+
             } catch(exception $e) {
 
                 $result['success'] = false;
@@ -28,6 +33,23 @@
 
                 echo json_encode($result, JSON_PRETTY_PRINT|JSON_UNESCAPED_UNICODE);
 
+                $connection = new AMQStreamConnection('203.250.32.181', 5672, guest, guest);
+                $channel = $connection->channel();
+
+                $channel->exchange_declare('logs', 'fanout', false, false, false);
+
+                $data = implode(' ', array_slice($argv, 1));
+
+                if(empty($data)) $data = "info: Hello World!";
+                $msg = new AMQPMessage($data);
+
+
+                $channel->basic_publish($msg, 'logs');
+
+                echo "[x] Sent ", $data, "\n";
+
+                $channel->close();
+                $connection->close();
             }
         ?>
     </body>
