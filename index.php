f2581c24353602bc23f232f4dd2bbb560c2d07e6
Geol2 <big9401@gmail.com>
2018-04-09 22:43:00 +0900

    send.php temp3

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <html>
 	<head>
 		<meta name="viewport" content="width=device-width">
-        <script src="https://code.jquery.com/jquery-1.12.4.min.js" integrity="sha256-ZosEbRLbNQzLpnKIkEdrPv7lOy9C27hHQ+Xp8a4MxAQ=" crossorigin="anonymous"></script>
+        <script src="https://code.jquery.com/jquery-1.12.4.min.js"></script>
         <title>Control</title>
 	</head>
 
