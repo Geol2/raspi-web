65e3fa082836b43408e7bf355f5771a3fac7f509
Geol2 <big9401@gmail.com>
2018-04-09 22:46:18 +0900

    send.php temp4

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <html>
 	<head>
 		<meta name="viewport" content="width=device-width">
-        <script src="https://code.jquery.com/jquery-1.12.4.min.js"></script>
+        <script src="https://code.jquery.com/jquery-1.12.4.min.js" integrity="sha256-ZosEbRLbNQzLpnKIkEdrPv7lOy9C27hHQ+Xp8a4MxAQ=" crossorigin="anonymous"></script>
         <title>Control</title>
 	</head>
 
@@ -13,7 +13,7 @@
         // ajax로 값 전달
         function goActEvent() {
             $.ajax({
-                url				: 'send.php',
+                url				: '/send.php',
                 type			: 'POST',
                 //dataType		: 'json',
                 success		: function(result) {
