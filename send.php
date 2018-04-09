0e0e729ceea68415b60b8c0cab4041df004f91d3
Geol2 <big9401@gmail.com>
2018-04-10 00:16:24 +0900

    asdasd

--- send.php
+++ send.php
@@ -1,34 +1,34 @@
 <!DOCTYPE html>
 <html>
-<head>
-    <meta charset="UTF-8" name="viewport" content="width=device-width">
-    <title>send</title>
-</head>
+    <head>
+        <meta charset="UTF-8" name="viewport" content="width=device-width">
+        <title>send</title>
+    </head>
 
-<body>
-<?php
-require_once __DIR__ . '/vendor/autoload.php';
-use PhpAmqpLib\Connection\AMQPStreamConnectionConnection;
-use PhpAmqpLib\Message\AMQPMessage;
-define('HOST','192.168.0.23');
-define('PORT',5672);
-define('USER','guest');
-define('PASS','guest');
+    <body>
+        <?php
+        require_once __DIR__ . '/vendor/autoload.php';
+        use PhpAmqpLib\Connection\AMQPStreamConnection;
+        use PhpAmqpLib\Message\AMQPMessage;
+        define('HOST','192.168.0.23');
+        define('PORT',5672);
+        define('USER','guest');
+        define('PASS','guest');
 
-$connection = new \PhpAmqpLib\Connection\AMQPStreamConnection(HOST, PORT, USER, PASS);
-$channel = $connection->channel();
+        $connection = new AMQPStreamConnection(HOST, PORT, USER, PASS);
+        $channel = $connection->channel();
 
-$channel->queue_declare('myQueue', false, false, false, false);
-$channel->exchange_declare('amq.direct', 'direct');
+        $channel->queue_declare('myQueue', false, false, false, false);
+        $channel->exchange_declare('amq.direct', 'direct');
 
-$temp = ['id'=>'0', 'temp'=> '10'];
-$data = json_encode($temp);
+        $temp = ['id'=>'0', 'temp'=> '10'];
+        $data = json_encode($temp);
 
-$msg = new AMQPMessage($data, array('content_type' => 'application/json'));
-$channel->basic_publish($msg,'amq.direct','foo.bar');
+        $msg = new AMQPMessage($data, array('content_type' => 'application/json'));
+        $channel->basic_publish($msg,'amq.direct','foo.bar');
 
-$channel->close();
-$connection->close();
-?>
-</body>
+        $channel->close();
+        $connection->close();
+        ?>
+    </body>
 </html>
