f70d43e47576fe8d2bfb3c6ed7d0bc4a4feb4a08
Geol2 <big9401@gmail.com>
2018-04-10 00:11:08 +0900

    1. connect

--- send.php
+++ send.php
@@ -6,14 +6,25 @@
 
     <body>
         <?php
-            require_once __DIR__ . '/vendor/autoload.php';
-            use PhpAmqpLib\Connection\AMQPStreamConnection;
-            use PhpAmqpLib\Message\AMQPMessage;
+            $cnn = new AMQPConnection();
 
-            define('HOST','192.168.0.23');
-            define('PORT', 5672);
-            define('USER','guest');
-            define('PASS','guest');
+            $cnn ->setLogin('guest');
+            $cnn ->setPassword('guest');
+
+            if($cnn->connect()){
+                echo "Establaished a connection to the broker";
+            }
+            else {
+                echo "Cannot connect to the broker.";
+            }
+            //require_once __DIR__ . '/vendor/autoload.php';
+            //use PhpAmqpLib\Connection\AMQPStreamConnection;
+            //use PhpAmqpLib\Message\AMQPMessage;
+
+            //define('HOST','192.168.0.23');
+            //define('PORT', 5672);
+            //define('USER','guest');
+            //define('PASS','guest');
             //try
             //{
             //   $param1 = $_POST['param1'];
@@ -37,21 +48,21 @@
 
             //echo json_encode($result, JSON_PRETTY_PRINT|JSON_UNESCAPED_UNICODE);
 
-            $connection = new AMQPStreamConnection(HOST, PORT, USER, PASS);
-            $channel = $connection->channel();
+            //$connection = new AMQPStreamConnection(HOST, PORT, USER, PASS);
+            //$channel = $connection->channel();
 
-            $channel->queue_declare('myQueue', false, true, false, false);
-            $channel->exchange_declare('amq.direct', 'direct', false, true, false);
-            $channel->queue_bind('myQueue','amq.direct');
+            //$channel->queue_declare('myQueue', false, true, false, false);
+            //$channel->exchange_declare('amq.direct', 'direct', false, true, false);
+            //$channel->queue_bind('myQueue','amq.direct');
 
-            $temp = ['id'=>'0', 'temp'=> '10'];
-            $data = json_encode($temp);
+            //$temp = ['id'=>'0', 'temp'=> '10'];
+            //$data = json_encode($temp);
 
-            $msg = new AMQPMessage($data, array('content_type' => 'application/json'));
-            $channel->basic_publish($msg,'amq.direct');
+            //$msg = new AMQPMessage($data, array('content_type' => 'application/json'));
+            //$channel->basic_publish($msg,'amq.direct');
 
-            $channel->close();
-            $connection->close();
+            //$channel->close();
+            //$connection->close();
 
             //}
         ?>
