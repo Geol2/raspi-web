e28f35934fbc55f5864626b1b056a67ddde07d23
Geol2 <big9401@gmail.com>
2018-04-10 00:32:22 +0900

    aaaaaaaaaaaaaaaaaaa

--- send.php
+++ send.php
@@ -2,10 +2,10 @@
 require_once __DIR__ . '/vendor/autoload.php';
 use PhpAmqpLib\Connection\AMQPStreamConnection;
 use PhpAmqpLib\Message\AMQPMessage;
-define('HOST', '192.168.0.23');
-define('PORT', 5672);
-define('USER', 'guest');
-define('PASS', 'guest');
+define("HOST", "192.168.0.23");
+define("PORT", 5672);
+define("USER", 'guest');
+define("PASS", "guest");
 
 $connection = new AMQPStreamConnection(HOST, PORT, USER, PASS);
 $channel = $connection->channel();
