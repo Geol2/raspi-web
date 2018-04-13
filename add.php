ce80b47185863acb6e7e56f8733da2516d87256a
Geol2 <big9401@gmail.com>
2018-04-14 06:47:55 +0900

    sdfsdf

--- add.php
+++ add.php
@@ -21,16 +21,18 @@
 		$info = json_decode(stripcslashes($request_body), true);
 		$user_code = $info['user_code'];
 
+		echo $ip;
+		echo $user_code;
         //echo $user_code;
 		//file_put_contents("user_code.json", json_encode(array('user_code' => $user_code), JSON_PRETTY_PRINT) );
 
-        $query = "INSERT INTO Sys_info( PUBLIC IP, USER_CODE ) VALUES ('$ip','$user_code')";
-        $result = mysqli_query($conn, $query) or die ('Error database.');
+        //$query = "INSERT INTO Sys_info( PUBLIC IP, USER_CODE ) VALUES ('$ip','$user_code')";
+        //$result = mysqli_query($conn, $query) or die ('Error database.');
 
-        $key = ['result'=>'OK'];
-		echo json_encode($key);
+        //$key = ['result'=>'OK'];
+		//echo json_encode($key);
 
-		mysqli_close($conn);
+		//mysqli_close($conn);
 	//}
 
 //	if($method == "GET"){
