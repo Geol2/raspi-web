684465c55e52c6c72983428da5ed0ec3a110b0dc
Geol2 <big9401@gmail.com>
2018-04-14 06:32:47 +0900

    sfad

--- add.php
+++ add.php
@@ -14,7 +14,7 @@
 
 		//chmod("./var/www/html/user_code.json", 777);
 
-		$request_body = $_POST['user_code']; //file_get_contents("php://input");
+		$request_body = file_get_contents("php://input");
 		//$info = json_decode($request_body, true);
 		$info = json_decode(stripcslashes($request_body), true);
 		$user_code = $info['user_code'];
