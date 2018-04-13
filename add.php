c192c5852970146332e2ceb7728f28ad6ea25e43
Geol2 <big9401@gmail.com>
2018-04-14 07:21:14 +0900

    ssssss

--- add.php
+++ add.php
@@ -22,11 +22,11 @@
 		$user_code = $info['user_code'];
 		$submit_ip = $info['submit_ip'];
 
-		echo $submit_ip;
+		//echo $submit_ip;
         //echo $user_code;
 		//file_put_contents("user_code.json", json_encode(array('user_code' => $user_code), JSON_PRETTY_PRINT) );
 
-        $query = "INSERT INTO Sys_info( PUBLIC_IP, USER_CODE ) VALUES ('$submit_ip','$user_code')";
+        $query = "INSERT INTO Sys_info( PUBLIC_IP, USER_CODE ) VALUES ('203.250.32.245', '$user_code')";
         $result = mysqli_query($conn, $query) or die ('Error database.');
 
         $key = ['result'=>'OK'];
