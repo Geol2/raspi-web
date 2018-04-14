3b309c60e410a8603d5e87ab5f9d1f1735d12d47
Geol2 <big9401@gmail.com>
2018-04-14 22:26:32 +0900

    eeeee

--- search.php
+++ search.php
@@ -30,22 +30,22 @@
     header('Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept');
     header('Content-Type: application/json');
 
-    $db_host = "localhost";
-    $db_user = "root";
-    $db_passwd = "619412";
-    $db_name = "water_middle_server";
+    $db_host1 = "localhost";
+    $db_user1 = "root";
+    $db_passwd1 = "619412";
+    $db_name1 = "water_middle_server";
 	//$str1 = file_get_contents('/var/www/html/user_code.json');
 
 	//$json = json_decode($str1, true);
-    $conn_user = mysqli_connect($db_host, $db_user, $db_passwd, $db_name) or die("Connected Failed!!!!");
+    $conn_user = mysqli_connect($db_host1, $db_user1, $db_passwd1, $db_name1) or die("Connected Failed!!!!");
 
     $query_user = "SELECT COUNT(*) FROM Sys_info";
-	$result_user = mysqli_query($conn_user, $query_user) or die ('Error Query_user Databases.');
-	//true 참 1, false 거짓 0
+	$result_user = mysqli_query($conn_user, $query_user);
+	//true 참 0 이외의 값 , false 거짓 0
 
 
 	$res = 'OK';
-	if( $result_user ){
+	if( $result_user != 0 ){
 		 $res = 'FAIL';
 	}
 	$data = ['state'=> $res ,'ssid' => 'pi3-ap' ,'inner_ip' => $return_arr ];
