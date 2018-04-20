dea880e9fce64a3c037204e512042b6a07a5bb5b
Geol2 <big9401@gmail.com>
2018-04-21 00:49:54 +0900

    remove 나눔

--- remove_pro.php
+++ remove_pro.php
@@ -10,17 +10,22 @@
 
     $conn = mysqli_connect($db_host, $db_user, $db_passwd, $db_name) or die("Connected Failed!!!!");
 
+    # Get JSON as a string
+    $json_str = file_get_contents('php://input');
 
-    $query = "DELETE FROM Sys_info WHERE USER_CODE = 0";
+    # Get as an object
+    $json_obj = json_decode($json_str);
+
+    $query = "DELETE FROM product_info";
     $result = mysqli_query($conn, $query) or die ('Error Querying database.');
 
-        //$str = file_get_contents('/var/www/html/user_code.json');
+    echo "Delete product_info";
+    //$str = file_get_contents('/var/www/html/user_code.json');
 
-	//$json = json_decode($str, true);
+    //$json = json_decode($str, true);
 
-	//if( array_key_exists("user_code", $json)) {
-	//	unlink('/var/www/html/user_code.json');
-	//}
+    //if( array_key_exists("user_code", $json)) {
+    //	unlink('/var/www/html/user_code.json');
+    //}
 
 ?>
-
