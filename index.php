a08793d714981ebbe716b3a81b509694f6025637
Geol2 <big9401@gmail.com>
2018-05-06 02:39:41 +0900

    test

--- index.php
+++ index.php
@@ -34,11 +34,8 @@
         $query = "INSERT INTO product_info  VALUES ('$ip', '$led', '$state', '$register')";
         $result_ip = mysqli_query($conn, $query) or die ('Error database.. not connect product table.');
         echo '  Customer added.'; echo "</br>";
-        mysqli_close($conn);
-
 
         // 아랫줄부터 user_code의 존재여부를 확인 후 POST 방식으로 전송함.
-        $conn = mysqli_connect($db_host, $db_user, $db_passwd, $db_name) or die("Connected Database Failed!!!!");
         $query_user_code = "SELECT * FROM Sys_info";
         $result_user = mysqli_query($conn, $query_user_code) or die ("Error database.. not connect Sys_info table.");
         // true 참 0 이외의 값, false 거짓 0
