899c1278e80bdd9315dc5c35783bab835de9498b
Geol2 <big9401@gmail.com>
2018-05-07 00:28:19 +0900

    test

--- index.php
+++ index.php
@@ -57,20 +57,26 @@
 
             $url = "203.250.32.180:9001/device/add/sf/auto"; echo $url; echo "</br>";
 
-            $ch = curl_init(); echo $ch; echo "</br>";
-            curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-            curl_setopt($ch, CURLOPT_POSTFIELDS, $json);
-            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-            curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-                    'Content-Type: application/json',
-                    'Content-Length: ' . strlen($json))
-            );
+            $curl = curl_init($url);
+            curl_setopt($curl, CURLOPT_HEADER, false);
+            curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($curl, CURLOPT_HTTPHEADER,
+                array("Content-type: application/json"));
+            curl_setopt($curl, CURLOPT_POST, true);
+            curl_setopt($curl, CURLOPT_POSTFIELDS, $json);
 
-            $result = curl_exec($ch);
+            $json_response = curl_exec($curl);
 
-            # Print response.
-            echo "<pre>$result</pre>";
+            $status = curl_getinfo($curl, CURLINFO_HTTP_CODE);
 
+            if ( $status != 201 ) {
+                die("Error: call to URL $url failed with status $status, response $json_response, curl_error " . curl_error($curl) . ", curl_errno " . curl_errno($curl));
+            }
+
+
+            curl_close($curl);
+
+            $response = json_decode($json_response, true);
         }
 
         else {
