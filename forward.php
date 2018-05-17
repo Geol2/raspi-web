b83f42c5c8909c41af52391ed05e27402cc0c1f0
Geol2 <big9401@gmail.com>
2018-05-17 18:51:54 +0900

    test

--- forward.php
+++ forward.php
@@ -31,16 +31,20 @@ $conn = mysqli_connect($db_host, $db_user, $db_passwd, $db_name) or die("Connect
     $key = ['cmd' => $cmd, 'dest' => $dest]; // 받아온 cmd, userCode 값을 key에 넣음.
     //echo json_encode($key);
 
-    $query = "SELECT USER_CODE FROM Sys_info";
+    $query = "SELECT USER_CODE FROM Sys_info WHERE $user_code";
     $result = mysqli_query($conn, $query) or die ("Error Database connect!!");
 
     while($data = mysqli_fetch_array($result)){
-        //print_r($data); // 유져코드 출력 완료.
+
+        print_r($data); // 유져코드 출력 완료. ( 배열로 출력됨. )
+        
+        /*
         if( $user_code == $data) {
             echo "success";
         }
         else {
-            echo "fail";    
+            echo "fail";
         }
+        */
     }
     ?>
