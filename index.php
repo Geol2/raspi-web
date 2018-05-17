880aaa543c23dfd93e4676a0643ee71f1363e719
Geol2 <big9401@gmail.com>
2018-05-17 15:52:31 +0900

    test naver

--- index.php
+++ index.php
@@ -62,18 +62,16 @@
             );
 
             $ch = curl_init();
-            curl_setopt ($ch, CURLOPT_URL,"http://www.naver.com"); //접속할 URL 주소
-            curl_setopt ($ch, CURLOPT_SSL_VERIFYPEER, FALSE); //인증서 체크. true 시 안되는 경우가 많다.
 
-            // default 값이 true 이기때문에 이부분을 조심 (https 접속시에 필요)
-            curl_setopt ($ch, CURLOPT_SSLVERSION,3); // SSL 버젼 (https 접속시에 필요)
-            curl_setopt ($ch, CURLOPT_HEADER, 0); // 헤더 출력 여부
-            curl_setopt ($ch, CURLOPT_TIMEOUT, 30); // TimeOut 값
-            curl_setopt ($ch, CURLOPT_RETURNTRANSFER, 1); // 결과값을 받을것인지
-            $result = curl_exec ($ch);
+            // set URL and other appropriate options
+            curl_setopt($ch, CURLOPT_URL, "http://www.naver.com/");
+            curl_setopt($ch, CURLOPT_HEADER, 0);
 
-            echo $result;
-            curl_close ($ch);
+            // grab URL and pass it to the browser
+            curl_exec($ch);
+
+            // close cURL resource, and free up system resources
+            curl_close($ch);
         }
 
         else {
