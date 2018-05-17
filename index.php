6203bd45fc4ab643a559356ccaa738c00c29f58d
Geol2 <big9401@gmail.com>
2018-05-17 15:07:14 +0900

    curl_init

--- index.php
+++ index.php
@@ -3,7 +3,6 @@
 // 아두이노에서 오는 IP를 받아서 자동으로 product_info 테이블에 장비를 추가해주는 코드. -->
 // userCode를 이용하여 데이터의 내용을 URL(203.250.32.180:9001/device/sf/auto)로 전송해야함 -->
 
-
     $db_host = "localhost";
     $db_user = "root";
     $db_passwd = "619412";
@@ -15,7 +14,6 @@
 <?php
     require_once __DIR__ .'/vendor/autoload.php';
 
-    use Middle;
 
     $ip = $_GET['ip']; //Query_string
     //$site = $_SERVER['DOCUMENT_ROOT']; //index.php road
@@ -52,32 +50,21 @@
             $exist_query = "SELECT * FROM Sys_info";
             $exist_result = mysqli_query($conn, $exist_query) or die ("Error database.. not connect Sys_info 2 table.");
 
-            $row = mysqli_fetch_array($exist_result, MYSQLI_BOTH);
+            $row = mysqli_fetch_array($exist_result, MYSQLI_ASSOC);
 
             $user_code = $row['USER_CODE']; //user_code를 변수에 넣음.
             $sys_info_ip = $row['OUTER_IP']; //user_code의 ip를 변수에 넣음.
 
             $fields = array(
-                    'apInfo' => urlencode($sys_info_ip),
-                    'ipInfo' => urlencode($ip),
-                    'userCode' => urlencode($user_code)
+                    'apInfo' => $sys_info_ip,
+                    'ipInfo' => $ip,
+                    'userCode' => $user_code
             );
 
-            foreach($fields as $key=>$value) { $fields_string .= $key.'='.$value.'&'; }
-            $fields_string = rtrim($fields_string,'&');
+            $url  = '203.250.32.180:9001/device/sf/auto';
+            $ch = curl_init($url);
 
-            
-            //open connection
-            $ch = curl_init();
-
-            //set the url, number of POST vars, POST data
-            curl_setopt($ch,CURLOPT_URL,$url);
-            curl_setopt($ch,CURLOPT_POST,count($fields));
-            curl_setopt($ch,CURLOPT_POSTFIELDS,$fields_string);
-
-            //execute post
-            $result = curl_exec($ch);
-            print $result;
+            echo $ch;
         }
 
         else {
@@ -90,9 +77,6 @@
     else {
         echo "Please get ip...";
     }
-
-
-
 
     //chmod("./var/www/html/inner_ip.json", 777);
 
