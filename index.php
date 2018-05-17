8e5ea07a60157bcac5b173c3285b03f74cf2add6
Geol2 <big9401@gmail.com>
2018-05-17 15:25:14 +0900

    test naver

--- index.php
+++ index.php
@@ -61,10 +61,18 @@
                     'userCode' => $user_code
             );
 
-            $url  = '203.250.32.180:9001/device/sf/auto';
-            $ch = curl_init($url);
+            $ch = curl_init();
+            curl_setopt ($ch, CURLOPT_URL,"http://www.naver.com"); //접속할 URL 주소
+            curl_setopt ($ch, CURLOPT_SSL_VERIFYPEER, FALSE); //인증서 체크같은데 true 시 안되는 경우가 많다.
 
-            echo $ch;
+            // default 값이 true 이기때문에 이부분을 조심 (https 접속시에 필요)
+            curl_setopt ($ch, CURLOPT_SSLVERSION,3); // SSL 버젼 (https 접속시에 필요)
+            curl_setopt ($ch, CURLOPT_HEADER, 0); // 헤더 출력 여부
+            curl_setopt ($ch, CURLOPT_TIMEOUT, 30); // TimeOut 값
+            curl_setopt ($ch, CURLOPT_RETURNTRANSFER, 1); // 결과값을 받을것인지
+            $result = curl_exec ($ch);
+            curl_close ($ch);
+            echo $result;
         }
 
         else {
