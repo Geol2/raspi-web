f258fa2c6b4b44c5dcb757928084104b084efcc9
Geol2 <big9401@gmail.com>
2018-05-25 01:18:13 +0900

    test

--- add.php
+++ add.php
@@ -37,8 +37,8 @@
 	$query = "INSERT INTO Sys_info (USER_CODE, OUTER_IP ) VALUES ( $user_code, '$submit_ip')";
 	$result = mysqli_query($conn, $query) or die ('Error database.');
 
-	$sf_key = ['user_code' => $user_code, 'sf_code' => [ 'ip'=> $ip ,'code'=> $code ]];
-	echo json_encode( $sf_code );
+	$sf_key = ['user_code' => $user_code, 'sf_code' => [ 'ip' => $ip , 'code' => $code ] ];
+	echo json_encode( $sf_key );
 
     $key = ['result'=>'OK'];
 	echo json_encode( $key );
