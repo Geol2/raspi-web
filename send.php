f48845fd88718ec2382fdfb1d40679c5d9085674
Geol2 <big9401@gmail.com>
2018-05-24 21:31:33 +0900

    test

--- send.php
+++ send.php
@@ -34,6 +34,8 @@
     $row = mysqli_fetch_array($result, MYSQLI_BOTH);
 
     $user_code = $row['USER_CODE'];
+
+    date_default_timezone_set('Asia/Seoul'); // 분침 -30분 문제..
     $current_time = date("Y/m/d/H/i"); //날짜 시간 추가..
     echo $current_time;
 
