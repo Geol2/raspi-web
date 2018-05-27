3c86465cf04103dd741a2b7a0d2e070ca30fbccc
Geol2 <big9401@gmail.com>
2018-05-27 22:25:54 +0900

    test

--- index.php
+++ index.php
@@ -75,11 +75,11 @@
             $result = curl_exec($c); //
 
             $json = json_decode($result, true); //json 데이터를 배열로 담기.
-            //print_r($json); // 배열을 출력.
+            print_r($json); // 배열을 출력.
 
-            //echo $json['status']."<br/>";
-            //echo $json['msg']."<br/>";
-            //echo $json['data']."<br/>";
+            echo $json['status']."<br/>";
+            echo $json['msg']."<br/>";
+            echo $json['data']."<br/>";
 
             //json 데이터 뿌리기
             foreach ($json as $key => $value){
