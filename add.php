c50580a0607dfae27000d1f3e4ed542ca0a76ea7
Geol2 <big9401@gmail.com>
2018-06-27 22:10:37 +0900

    test

--- add.php
+++ add.php
@@ -1,52 +1,24 @@
-
 <?php
-//API서버에 AP가 등록된 후, AP가 어떤 사용자에게 등록되었는지에 대한 정보를 저장
 //Sys_info 를 웹서버에서 수동으로 추가해주는 부분.
 //자동으로 추가해주는 부분을 만들어 보자.
-//sf_code 추가.
-//index.php에서 innerIp가 들어가 있는 상태에서 공유기를 추가.>??
-
-
 	header("Access-Control-Allow-Origin: *");
 	header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
 	header("Content-Type: application/json");
-
 	$db_host = "localhost";
 	$db_user = "root";
 	$db_passwd = "619412";
 	$db_name = "water_middle_server";
-
 	$conn = mysqli_connect($db_host, $db_user, $db_passwd, $db_name) or die("Connected Failed!!!!");
 	# Get JSON as a string
 	$json_str = file_get_contents('php://input');
-
 	# Get as an object
-	$json_obj = json_decode($json_str, false); // false : object 로 반환.
-                                                    // true : array로 반환.
-
-    // 실제로 받는 값들.. {"user_code" : " ", "submit_ip" : " ", "sf_code": [ {"code" : " ", "ip" : " " } ] }
-	$user_code = $json_obj->{"user_code"};
-	$submit_ip = $json_obj->{"submit_ip"};
-
-    //json 데이터 뿌리기
-    foreach ($json_obj->{"sf_code"} as $key => $value) {
-        $data_code = $value->{"code"};
-        $data_ip = $value->{"ip"};
-
-        $query_sf = "UPDATE product_info SET sf_code = $data_code WHERE INNER_IP = '$data_ip'";
-        $result_sf = mysqli_query($conn, $query_sf) or die ('Error insert product_table table.');
-    }
-
-
-
-	$query = "INSERT INTO Sys_info (USER_CODE, OUTER_IP ) VALUES ( $user_code, '$submit_ip')";
-	$result = mysqli_query($conn, $query) or die ('Error insert Sys_info table.');
-
-    //echo json_encode($sf_key);// user_code 와 sf_code 안의 ip, code를 받아오는 부분을 출력한다. php의 배열은 동적이라 따로 정해두지 않아도 알아서 출력이 된다고 한다.
-
-    $key = ['result'=>'OK'];
-	echo json_encode( $key );
+	$json_obj = json_decode($json_str);
+	$ap_code = $json_obj->{"ap_code"};
+	$public_ip = $json_obj->{"public_ip"};
+	$query = "INSERT INTO Sys_info (USER_CODE, PUBLIC_IP ) VALUES ( $ap_code, '$public_ip')";
+	$result = mysqli_query($conn, $query) or die ('Error database.');
+	$key = ['result'=>'OK'];
+	echo json_encode($key);
     //Sys_info 의 user_code와 ouer_ip를 받아서 DB에 저장.
-
     mysqli_close($conn);
 ?>
--- index.php
+++ index.php
@@ -1,130 +1,79 @@
-
 <?php
-// 수경재배기랑 AP최초 연결 시, 수경재배기 데이터 mysql db에 저장
 // 아두이노에서 오는 IP를 받아서 자동으로 product_info 테이블에 장비를 추가해주는 코드. -->
 // userCode를 이용하여 데이터의 내용을 URL(203.250.32.157:9001/device/sf/auto)로 전송해야함 -->
-
     $db_host = "localhost";
     $db_user = "root";
     $db_passwd = "619412";
     $db_name = "water_middle_server";
-
     $conn = mysqli_connect($db_host, $db_user, $db_passwd, $db_name) or die("Connected Database Failed!!!!");
 ?>
 
 <?php
     //require_once __DIR__ .'/vendor/autoload.php';
-
-
     $ip = $_GET['ip']; //Query_string
     //$site = $_SERVER['DOCUMENT_ROOT']; //index.php road
     //$self_ip = $_SERVER['SERVER_ADDR']; //my ip
     //$whois_user = $_SERVER['REMOTE_ADDR']; //웹서버의 요청을 보내는 사용자ip
     //$using_port = $_SERVER['SERVER_PORT']; //클라이언트 포트
-
     //printf("Root : %s<br/>", $site);
     //printf("SERVER_IP : %s<br/>",$self_ip);
     //printf("USER_IP : %s<br/>", $whois_user);
     //printf("PORT : %s<br/>", $using_port);
     echo "QUERY_STRING_IP : ".$ip; echo "</br>";
-
     if( $ip ){
-        //echo "get ip<br/>";
-        $led = 'N'; // led 수동'n' / 자동'y' 모드
-        $state = 'N'; // 수경재배기 펌프 최초 작동 상태. 'n' : 작동x, 'y':작동o.
+        $sfCode= split(".",$money)[3];//$ip로부터 ip의 D class추출
+        $mode = 'Y'; // mode 수동'n' / 자동'y' 모드
+        $state = 'N'; // 수경재배기 생육 상태. 'n' : 생육x, 'y':셍육o.
         $register = 'N'; //등록 상태
 
-        $query = "INSERT INTO product_info (INNER_IP, LED_MODE, STATE_, REGISTER, sf_code) VALUES ('$ip', '$led', '$state', '$register', 'NULL' )";
+        $query = "INSERT INTO product_info (INNER_IP, MODE, STATE, REGISTER, SF_CODE) VALUES ('$ip', '$mode', '$state', '$register', '$sfCode')";
         $result_ip = mysqli_query($conn, $query) or die ('Error database.. not connect product table.');
-        echo 'Customer added.'; echo "</br>";
-
+        echo '  Customer added.'; echo "</br>";
         // 아랫줄부터 user_code의 존재여부를 확인 후 POST 방식으로 전송함.
-        $query_user = "SELECT * FROM Sys_info"; //echo $query; echo "</br>";
-        $result_user = mysqli_query($conn, $query_user) or die ("Error database.. not connect Sys_info table.");
+        $query_user = "SELECT COUNT(*) FROM Sys_info";
+        $count = mysqli_query($conn, $query_user) or die ("Error database.. not connect Sys_info table.");
         // true 참 0 이외의 값, false 거짓 0
-
-        $num = mysqli_num_rows($result_user); //echo "$num";
         //Sys_info의 table 행 개수 저장.
-
-        if( $num >= 1) {
-            //user_code가 존재한다면.
+        
+        if( $count == 1) {//user_code가 존재한다면.
             $exist_query = "SELECT * FROM Sys_info";
             $exist_result = mysqli_query($conn, $exist_query) or die ("Error database.. not connect Sys_info 2 table.");
-
             $row = mysqli_fetch_array($exist_result, MYSQLI_ASSOC);
-
-            $user_code = $row['USER_CODE']; //user_code를 변수에 넣음.
-            $sys_info_ip = $row['OUTER_IP']; //user_code의 ip를 변수에 넣음.
-
+            $ap_code = $row['AP_CODE']; //ap_code를 변수에 넣음.
             $fields = array(
-                    'apInfo' => $sys_info_ip,
+                    'sfCode' => $sfCode,
                     'ipInfo' => $ip,
-                    'userCode' => $user_code
+                    'apCode' => $ap_code
             );
-
-            $url = 'http://203.250.32.43:9001/smart_plant/device/add/sf/auto'; //이것도 변경 가능성이 있음.
-
+            $url = 'http://203.250.32.47:9001/smart_plant/device/add/sf/auto'; //이것도 변경 가능성이 있음.
             $c = curl_init($url);
-
             curl_setopt($c, CURLOPT_RETURNTRANSFER, true); // 요청 설정을 POST로 한다.
             curl_setopt($c, CURLOPT_POST, true); // 요청을 JSON으로 전달하는 헤더 설정.
             curl_setopt($c, CURLOPT_HTTPHEADER, array('Content-Type: application/json')); //전송할 데이터를 JSON으로 가공하기.
-            curl_setopt($c, CURLOPT_POSTFIELDS, json_encode($fields)); //
-
-            //print curl_exec($c); // ex.. {"status":"OK","msg":"success","data":{"code":129,"ip":"192.168.4.13"}}
-
-            $result = curl_exec($c); //
-
-            $json = json_decode($result, true); //json 데이터를 배열로 담기.
-            //print_r($json); // 배열을 출력.
-
-            //json 데이터 뿌리기
-            foreach ($json as $key => $value){
-                //echo $value['code']."<br/>";
-                ///echo $value['ip']."<br/>";
-
-                $data_code = $value['code'];
-                $data_ip = $value['ip'];
-            }
-
+            curl_setopt($c, CURLOPT_POSTFIELDS, json_encode($fields));
+            print curl_exec($c);
             curl_close($c);
-
-            $update1_query = "UPDATE product_info SET sf_code = $data_code WHERE INNER_IP = '$data_ip'";
-            $update1_result = mysqli_query($conn, $update1_query) or die ("Error database.. not connect product_info table.");
         }
-
         else {
             echo "Please user_code input..";
         }
-
         mysqli_close($conn);
     }
-
     else {
         echo "Please get ip...";
     }
-
     //chmod("./var/www/html/inner_ip.json", 777);
-
     //$is_file_exist = file_exists('/var/www/html/inner_ip.json');
-
     //	$iptables_version = shell_exec("sudo iptables --version");
     //	echo "<pre> $iptables_version </pre>";
-
     ///	$port_forward = exec("sudo iptables -t nat -A PREROUTING -p tcp -i eth0 --dport 9000 -j DNAT --to 192.168.4.19:80");
     //	echo "<pre> $port_forward </pre>";
-
     //	$port_save = shell_exec('sudo sh -c "iptables-save > /etc/tables/rules.v4"');
     //	echo "<pre> $port_save</pre>";
-
     //	$iptables_show = shell_exec("/etc/iptables sudo iptables -L");
     //	echo "<pre>$iptables_show</pre>";
-
     //	$data = shell_exec("ls");
     //	echo "<pre>$data</pre>";
-
     //	echo shell_exec("<pre>whoami</pre>");
-
     //	echo shell_exec("<pre>ls -al</pre>");
-
 ?>
