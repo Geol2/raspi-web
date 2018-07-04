f99fd67c3c3dbdf073aaa2ade68b0cfc6458cb7d
Geol2 <big9401@gmail.com>
2018-07-04 14:23:14 +0900

    test

--- send.php
+++ send.php
@@ -52,16 +52,16 @@
 
 
         // ap_code를 추출하여 $temp.에 삽입.
-        $query_ap = "SELECT AP_CODE FROM SYSINFO"; // 쿼리문 작성.
-        $result_ap = mysqli_query($link, $query_ap) or ("Not select SYS_INFO Table at AP_CODE"); // 쿼리문 실행.
+        //$query_ap = "SELECT AP_CODE FROM SYSINFO"; // 쿼리문 작성.
+        //$result_ap = mysqli_query($link, $query_ap) or ("Not select SYS_INFO Table at AP_CODE"); // 쿼리문 실행.
 
-        $ap_value = mysqli_fetch_array($result_ap, MYSQLI_BOTH); // 데이터 값을 표현해주는 방식을 ~만들고.
+        //$ap_value = mysqli_fetch_array($result_ap, MYSQLI_BOTH); // 데이터 값을 표현해주는 방식을 ~만들고.
 
-        $value = $ap_value['AP_CODE'];// row의 0행값을 뽑아서 ~저장.
+        //$value = $ap_value['AP_CODE'];// row의 0행값을 뽑아서 ~저장.
 
         //printf("%d", $value);
 
-        $temp = ['t' => $_GET['t'], 'h' => $_GET['h'], 'wt' => $_GET['wt'], 'wl' => $_GET['wl'], 'e' => $_GET['e'], 'd' => $current_time, 'sf' => 11, 'ap' => $value ];
+        $temp = ['t' => $_GET['t'], 'h' => $_GET['h'], 'wt' => $_GET['wt'], 'wl' => $_GET['wl'], 'e' => $_GET['e'], 'd' => $current_time, 'sf' => 11, 'ap' => 121 ];
         //$temp = ['t' => $_GET['t'], 'h' => $_GET['h'], 'wt' => $_GET['wt'], 'wl' => $_GET['wl'], 'e' => $_GET['e'], 'd' => $current_time, 'sf' => $_GET['sf'] ];
         // t: temperature
         // h : 습도
