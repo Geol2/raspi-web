bed968c715d602cfd07db320f1ddc1a2b3b75a8b
Geol2 <big9401@gmail.com>
2018-07-05 18:53:49 +0900

    test

--- send.php
+++ send.php
@@ -1,3 +1,32 @@
+<?php
+class Settings{
+    private static $instance;
+    private $settings;
+
+    private function __construct($ini_file) {
+        $this->settings = parse_ini_file($ini_file, true);
+    }
+
+    public static function getInstance($ini_file) {
+        if(! isset(self::$instance)) {
+            self::$instance = new Settings($ini_file);
+        }
+        return self::$instance;
+    }
+
+    public function __get($setting) {
+        if(array_key_exists($setting, $this->settings)) {
+            return $this->settings[$setting];
+        } else {
+            foreach($this->settings as $section) {
+                if(array_key_exists($setting, $section)) {
+                    return $section[$setting];
+                }
+            }
+        }
+    }
+}// end class;
+?>
 
 <?php
 // 수경재배기로부터 받아온 데이터를 JSON포맷 형태로 가공하여 rabbitMQ로 전송
