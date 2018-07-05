25c9f71e798c1dcf4faf0621e24776646536f686
Geol2 <big9401@gmail.com>
2018-07-05 18:42:56 +0900

    test

--- index.php
+++ index.php
@@ -38,7 +38,7 @@
     }// end class;
 
     $ip_url_settings = Settings::getInstance('php.ini');
-    $ip_url_setting = $ip_url_settings->ip_url;
+    //$ip_url_setting = $ip_url_settings->ip_url;
 
     //require_once __DIR__ .'/vendor/autoload.php';
     $ip = $_GET['ip']; //Query_string
