469133cb3e91bffae6814427f73c4097980ebd6e
Geol2 <big9401@gmail.com>
2018-07-20 02:32:15 +0900

    test

--- login_check.php
+++ login_check.php
@@ -10,18 +10,19 @@ include("config_db.php");
 session_start();
 
 $id = $_POST['id'];
-$pwd = $_POST['pwd'];
+$pwd = md5($_POST['pwd']);
 
 $check = "SELECT * FROM id WHERE id='$id'";
 $result = $conn->query($check);
 
-if($result->num_rows==1){
+if($result -> num_rows == 1){
     $row = $result->fetch_array(MYSQLI_ASSOC);
     if($row['userpwd'] == $pwd){
         $_SESSION['userid'] = $id;
 
-        if(isset($_SESSION['userid'])){
-            header('Location: ./main.php');
+        if(isset($_SESSION['userid']))
+        {
+            header('Location: ./main.php'); //로그인 성공시 페이지 이동.
         }
         else {
             echo "세션 저장 실패";
