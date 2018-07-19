db505b13d7e4e9a11254702f88f56e3f3fc27395
Geol2 <big9401@gmail.com>
2018-07-19 21:13:57 +0900

    test

--- login.php
+++ login.php
@@ -1,15 +1,18 @@
 <?php
-    
+    include ("config_db.php");
+    session_start();
 ?>
 
 <html>
     <head> </head>
 
     <body>
+    <h1>INPUT LOGIN </h1>
         <form method = "POST" action = "login.php">
             id <input type = "text" name = "id"> <br>
             pw <input type = "password" name = "pw"> <br>
             <input type = "submit" value = "login">
+            <input type = "submit" value = "sign up">
         </form>
     </body>
 </html>
