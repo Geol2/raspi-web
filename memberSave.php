3153e26dfbc0b286c689fc9306d6b517e3c3573a
Geol2 <big9401@gmail.com>
2018-07-20 01:32:15 +0900

    test

--- memberSave.php
+++ memberSave.php
@@ -10,7 +10,7 @@ include ("config_db.php");
 
 $id = $_POST['id'];
 $pwd = md5($_POST['pwd']);
-$pwd2 = $_POST['pwd2'];
+$pwd2 = md5($_POST['pwd2']);
 $email = $_POST['email'];
 
 $query = "INSERT INTO LOGIN_INFO (id, pwd, email) values ('$id', '$pwd', '$email')";
@@ -20,3 +20,25 @@ if($conn -> query($query)){
 } else {
     echo "fail login";
 }
+
+if($pwd != $pwd2) {
+    echo "비밀번호가 서로 다릅니다.";
+    echo "<a href = sign_up.php> back page</a>";
+    exit();
+}
+
+if($id == NULL || $pwd == NULL || $pwd2 == NULL || $email == NULL) {
+    echo "빈 칸을 모두 채워야 합니다.";
+    echo "<a href = sign_up.php> back page</a>";
+    exit();
+}
+
+$check = "SELECT * FROM LOGIN_INFO WHERE id='$id' ";
+
+$result = $conn->query($check);
+
+if($result -> num_rows == 1) {
+    echo "중복된 id입니다.";
+    echo "<a href='sign_up.php'> back page </a>"
+}
+
