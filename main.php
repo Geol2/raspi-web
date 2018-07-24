9b6a111247bd92298ffa805b1ed343681c66d012
Geol2 <big9401@gmail.com>
2018-07-24 14:52:32 +0900

    bootstrap

--- main.php
+++ main.php
@@ -5,16 +5,36 @@
  * Date: 2018-07-20
  * Time: 오전 2:17
  */
-session_start();
-if(!isset($_SESSION['userid']))
+
+function session_start()
 {
-    header('Location : ./login.php');
+    session_start();
+
+    if (!isset($_SESSION['userid'])) {
+        header('Location : ./login.php');
+    }
+
+    echo "홈(로그인) 성공</br>";
+    echo "<a href=logout.php>logout</a> </br>";
 }
 
-echo "홈(로그인) 성공</br>";
-echo "<a href=logout.php>logout</a> </br>";
-
-
-$ip_print = $_SERVER['REMOTE_ADDR'];
-echo "현재 접속한 장치의 아이피 : ".$ip_print."</br>";
+function ip_name()
+{
+    $ip_print = $_SERVER['REMOTE_ADDR'];
+    echo "현재 접속한 장치의 아이피 : " . $ip_print . "</br>";
+}
 ?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width", initial-scale="1">
+        <title>공유기 로그인 완료.</title>
+        <link rel="stylesheet" href="css/bootstrap.css">
+    </head>
+
+    <body>
+        Hello World!.
+    </body>
+</html>
