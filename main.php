6e8c6d11b7b5fe437fdabae3230330611f431c15
Geol2 <big9401@gmail.com>
2018-08-18 05:47:14 +0900

    test

--- main.php
+++ main.php
@@ -47,7 +47,7 @@
 
         <div class="container-fluid p-0">
             <section class="resume-section p-3 p-lg-5 d-flex d-column" id="about">
-                <h1 class="mb-0"> NAT LOGIN SUCCESS.</h1></br>
+                <h1 class="mb-0"> NAT LOGIN SUCCESS.<br> </h1>
                 <p> <a class="#" href="logout.php" role="button"> Logout </a></p>
             </section>
 
