4b33bf4882a337fddfd2f99f5e3b54e7e945e28c
Geol2 <big9401@gmail.com>
2018-08-18 06:52:51 +0900

    test

--- login.php
+++ login.php
@@ -1,3 +1,17 @@
+<?php
+header(' 
+    Cache-Control: no-store, 
+    no-cache, 
+    private, 
+    max-age=0, 
+    must-revalidate, 
+    post-check=0, 
+    pre-check=0; 
+    Pragma: no-cache; 
+    Expires: Sat, 26 jul 1997 05:00:00 GMT; 
+ ');
+?>
+
 <html>
     <head>
         <meta charset="utf-8">
