005e29096f4d2953b186e98c7dffb0df3980b3a1
Geol2 <big9401@gmail.com>
2018-08-18 06:35:41 +0900

    test

--- login.php
+++ login.php
@@ -1,3 +1,15 @@
+<?php
+if(strtolower(basename($_SERVER['SCRIPT_NAME']))=='login.php') {
+    header('Pragma: no-cache'); // HTTP/1.0
+    header('Cache-Control: no-cache'); // HTTP/1.1
+    header('Expires: -1'); // rfc2616 - Section 14.21
+    echo '<meta http-equiv="Cache-Control" content="no-cache"/>'.PHP_EOL
+        .'<meta http-equiv="Expires" content="-1"/>'.PHP_EOL
+        .'<meta http-equiv="Pragma" content="no-cache"/>'.PHP_EOL
+        .'<meta http-equiv="Pragma" content="no-store"/>'.PHP_EOL;
+}
+?>
+
 <html>
     <head>
         <meta charset="utf-8">
