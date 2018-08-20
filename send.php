8d09ca10ffc273cb98470cdee81479978f0cdb86
Geol2 <big9401@gmail.com>
2018-08-20 19:30:20 +0900

    test

--- send.php
+++ send.php
@@ -67,7 +67,7 @@
         //$ap_value = mysqli_fetch_array($result_ap, MYSQLI_BOTH); // 데이터 값을 표현해주는 방식을 ~만들고.
         //$value = $ap_value['AP_CODE'];// row의 0행값을 뽑아서 ~저장.
 
-        $temp = ['t' => $_GET['t'], 'h' => $_GET['h'], 'wt' => $_GET['wt'], 'wl' => $_GET['wl'], 'e' => $_GET['e'], 'd' => $current_time, 'sf' => $_GET['sf'], 'ap' => $value, 'ec' => $_GET['ec'], 'ph'=> $_GET['ph'], 'mip' => $_GET['mip']];
+        $temp = ['t' => $_GET['t'], 'h' => $_GET['h'], 'wt' => $_GET['wt'], 'wl' => $_GET['wl'], 'e' => $_GET['e'], 'd' => $current_time, 'sf' => $_GET['sf'], 'ap' => $value, 'ec' => $_GET['ec'], 'ph'=> $_GET['ph']];
         //$temp = ['t' => $_GET['t'], 'h' => $_GET['h'], 'wt' => $_GET['wt'], 'wl' => $_GET['wl'], 'e' => $_GET['e'], 'd' => $current_time, 'sf' => $_GET['sf'] ];
         // t: temperature
         // h : 습도
