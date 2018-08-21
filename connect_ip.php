90d3d724a0f2a80a492a5f99da90ca7af664ae36
Geol2 <big9401@gmail.com>
2018-08-22 04:07:18 +0900

    test

--- connect_ip.php
+++ connect_ip.php
@@ -1,5 +1,9 @@
 <?php
     $arr_ip = array_map(function ($n) { return sprintf('192.168.4.%d', $n); }, range(1, 20));
 
-    print_r($arr_ip);
+    print_r($arr_ip[0]);
+
+    if(preg_match("//",$_SERVER['REMOTE_ADDR'])){
+
+    }
 ?>
