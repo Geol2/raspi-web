4ad54f5982d9e7e35b14c1999c95a2585ca2ebea
Geol2 <big9401@gmail.com>
2018-08-22 05:18:19 +0900

    test

--- sign_up.php
+++ sign_up.php
@@ -88,7 +88,7 @@
                                 <input type="text" class="form-control" id="email_label" name="email" placeholder="E-MAIL" size="30">
                             </div>
                             <br>
-                            <button type="submit" class="btn btn-primary" id="join_submit" href="sign_up.php"> Sign Up </button>
+                            <button type="submit" class="btn btn-primary" id="join_submit" href="check_sign_up.php"> Sign Up </button>
                         </form>
 
                     </div>
@@ -107,57 +107,3 @@
         <script src="js/resume.min.js"></script>
     </body>
 </html>
-
-
-
-<?php
-/**
- * Created by PhpStorm.
- * User: big94
- * Date: 2018-07-19
- * Time: 오후 9:16
- */
-
-    include ("config_db.php");
-
-    $id = $_POST['id'];
-    $pwd = md5($_POST['pwd']);
-    $pwd2 = md5($_POST['pwd2']);
-    $email = $_POST['email'];
-
-    if($pwd != $pwd2) {
-        //echo "비밀번호가 서로 다릅니다.";
-        //echo "<a href = sign_up.php> back page</a>";
-    echo "<script>alert('회원가입 실패. 비밀번호가 서로 다릅니다.');</script>";
-    echo "<script>location.replace('/sign_up.php');</script>";
-    exit();
-    }
-
-    if($id == NULL || $pwd == NULL || $pwd2 == NULL || $email == NULL) {
-    //echo "빈 칸을 모두 채워야 합니다.";
-    //echo "<a href = sign_up.html> back page</a>";
-    echo "<script>alert('빈 칸을 채워야 합니다.');</script>";
-    echo "<script>location.replace('/sign_up.php');</script>";
-    exit();
-    }
-
-    $check_id = "SELECT * FROM LOGIN_INFO WHERE userid='$id' ";
-
-    $result = $conn->query($check_id);
-    if($result -> num_rows == 1) {
-    //echo "중복된 id입니다.";
-    //echo "<a href='sign_up.html'> back page </a>";
-    echo "<script>alert('중복된 아이디입니다.');</script>";
-    echo "<script>location.replace('/sign_up.php');</script>";
-    exit();
-    }
-
-    $query = mysqli_query($conn,"INSERT INTO LOGIN_INFO (userid, userpwd, useremail) VALUES ('$id', '$pwd', '$email')");
-    if($query) {
-    //echo "sign up success";
-    //echo "<script> document.location.href='/login.php'; </script>";
-    echo "<script>alert('회원가입 성공.');</script>";
-    echo "<script>location.replace('/login.php');</script>";
-}
-
-?>
