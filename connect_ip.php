d02a625e205bcf739cacc65846425dabff842dd4
Geol2 <big9401@gmail.com>
2018-08-22 02:53:33 +0900

    test

--- connect_ip.php
+++ connect_ip.php
@@ -1,8 +1,6 @@
 <?php
     $i = 0;
-    for($i=0; $i<=20; $i++){
-        $connect_ip = array("192.168.4." => $i);
-    }
-    print_r($connect_ip);
+    $connect_ip_d = array_fill(0, 20, $i);
 
+    print_r($connect_ip_d);
 ?>
