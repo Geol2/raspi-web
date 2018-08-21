ea1d43fe7845f00893fa36f5adac38bcf5b2bd55
Geol2 <big9401@gmail.com>
2018-08-22 04:41:40 +0900

    test

--- index.php
+++ index.php
@@ -10,6 +10,7 @@
 
 <?php
     require_once __DIR__ . '/path_ip_class.php';
+    require_once __DIR__ . 'connect_inner_ip.php';
 
     $ip_url_settings = Settings::getInstance('php.ini');
     $ip_setting = $ip_url_settings->ip;
