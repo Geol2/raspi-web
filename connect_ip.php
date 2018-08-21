396b9b80302570aceea54313494a020fbe648323
Geol2 <big9401@gmail.com>
2018-08-22 03:24:55 +0900

    tt

--- connect_ip.php
+++ connect_ip.php
@@ -1,11 +1,4 @@
 <?php
-    $ip_arr = [];
-
-    $ip_d = 1;
-    while( $ip_d <= 20 ){
-        $ip_arr = $ip_d;
-    }
-
-    $json_ip_arr = json_encode($ip_arr);
-    echo json_encode($json_ip_arr);
+    $ip_d = 0;
+    print $ip_d;
 ?>
