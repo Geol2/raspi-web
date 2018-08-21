3d3dba249b50e1d1f7df18d199cdbbc78b3eaacf
Geol2 <big9401@gmail.com>
2018-08-22 04:12:36 +0900

    test

--- connect_ip.php
+++ connect_ip.php
@@ -1,7 +1,12 @@
 <?php
     $arr_ip = array_map(function ($n) { return sprintf('192.168.4.%d', $n); }, range(1, 20));
 
-    print_r($arr_ip);
+    //print_r($arr_ip);
 
-    echo $_SERVER['REMOTE_ADDR'];
+    if(in_array($_SERVER['REMOTE_ADDR'], $arr_ip)){
+        echo "존재합니다.";
+    } else {
+        echo "존재하지 않습니다.";
+    }
+    //echo $_SERVER['REMOTE_ADDR'];
 ?>
