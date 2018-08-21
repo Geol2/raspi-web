6e32d75f2689139aa4258cffa55965ef7a508948
Geol2 <big9401@gmail.com>
2018-08-22 06:32:55 +0900

    test

--- remove_pro.php
+++ remove_pro.php
@@ -5,23 +5,23 @@
 
     require_once __DIR__ .'/path_ip_class.php';
 
-    header('Access-Control-Allow-Origin: * ');
-    header('Access-Control-Allow-Headers: Origin, X-Requested-With, Contentent-Type, Accept');
-    header('Content-Type: application/json');
-
-    $db_host = "localhost";
-    $db_user = "root";
-    $db_passwd = "619412";
-    $db_name = "water_middle_server";
-
-    $conn = mysqli_connect($db_host, $db_user, $db_passwd, $db_name) or die("Connected Failed!!!!");
-
-
     $ip_url_settings = Settings::getInstance('php.ini');
     $ip_setting = $ip_url_settings->ip_url;
 
     #접속 권한
     if($ip_setting == $_SERVER['REMOTE_ADDR']) {
+
+        header('Access-Control-Allow-Origin: * ');
+        header('Access-Control-Allow-Headers: Origin, X-Requested-With, Contentent-Type, Accept');
+        header('Content-Type: application/json');
+
+        $db_host = "localhost";
+        $db_user = "root";
+        $db_passwd = "619412";
+        $db_name = "water_middle_server";
+
+        $conn = mysqli_connect($db_host, $db_user, $db_passwd, $db_name) or die("Connected Failed!!!!");
+
         # Get JSON as a string
         $json_str = file_get_contents('php://input');
 
