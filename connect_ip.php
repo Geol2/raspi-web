f11a8c0c0c566a912d31fba2dcec2e1688cc1c2b
Geol2 <big9401@gmail.com>
2018-08-22 03:48:51 +0900

    test

--- connect_ip.php
+++ connect_ip.php
@@ -1,5 +1,5 @@
 <?php
-    $arr_ip = array_map(function ($n) {return sprintf('192.168.4.%2d', $n)}, range(1, 20));
+    $arr_ip = array_map(function ($n) { return sprintf('192.168.4.%2d', $n); }, range(1, 20));
 
     print_r($arr_ip);
 ?>
