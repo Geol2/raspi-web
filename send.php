441ccabadd04c6e92b1050bd1244cf7b8c50d561
big9401@gmail.com <big9401@gmail.com>
2018-11-22 16:46:05 +0900

    change.php

--- send.php
+++ send.php
@@ -67,8 +67,8 @@
         //$ap_value = mysqli_fetch_array($result_ap, MYSQLI_BOTH); // 데이터 값을 표현해주는 방식을 ~만들고.
         //$value = $ap_value['AP_CODE'];// row의 0행값을 뽑아서 ~저장.
 
-        $temp = ['t' => $_GET['t'], 'h' => $_GET['h'], 'wt' => $_GET['wt'], 'wl' => $_GET['wl'], 'e' => $_GET['e'], 'd' => $current_time, 'sf' => $_GET['sf'], 'ap' => $value, 'ec' => $_GET['ec'], 'ph'=> $_GET['ph']];
-        //$temp = ['t' => $_GET['t'], 'h' => $_GET['h'], 'wt' => $_GET['wt'], 'wl' => $_GET['wl'], 'e' => $_GET['e'], 'd' => $current_time, 'sf' => $_GET['sf'] ];
+        $temp = ['t' => $_GET['t'], 'h' => $_GET['h'], 'wt' => $_GET['wt'], 'wl' => $_GET['wl'], 'e' => $_GET['e'], 'd' => $current_time, 'sf' => $_GET['sf'], 'ap' => $value, 'ec' => $_GET['ec'], 'ph'=> $_GET['ph'], 'p' => $_GET['p']];
+        //$temp = ['t' => $_GET['t'], 'h' => $_GET['h'], 'wt' => $_GET['wt'], 'wl' => $_GET['wl'], 'e' => $_GET['e'], 'd' => $current_time, 'sf' => $_GET['sf'],'p' => $_GET['p']];
         // t: temperature
         // h : 습도
         // wt : 수온
